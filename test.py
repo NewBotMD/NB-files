48d9097a0a6e2992667571216d7aa53a71de3eec
Newbot <58357514+NewBotMD@users.noreply.github.com>
2019-12-09 12:49:44 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("helo")
+from utlis.rank import setrank,isrank,remrank,remsudos,setsudo, GPranks,IDrank
+from utlis.send import send_msg, BYusers, GetLink,Name,Glang
+from utlis.locks import st,getOR
+from utlis.tg import Bot
+from config import *
+
+from pyrogram import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
+import threading, requests, time, random, re, json
+import importlib
+
+from pyrogram.api.types import InputPeerChat
+def updateMsgs(client, message,redis):
+  print("fro me test.py :)")
