986bdff6773cb3f9a7971b3959d09a1d1da88164
Newbot <58357514+NewBotMD@users.noreply.github.com>
2020-01-22 14:02:44 -0800

    Update tr.py

--- tr.py
+++ tr.py
@@ -20,19 +20,19 @@ def updateMsgs(client, message,redis):
   type = message.chat.type
 
   if text and text == "نقل البيانات" and rank == "sudo":
-    if redis.smembers("thsake:gog"+BOT_ID):
-      Ngp = redis.scard("thsake:gog"+BOT_ID)
+    if redis.smembers(BOT_ID+"Tshake:Chek:Groups"):
+      Ngp = redis.scard(BOT_ID+"Tshake:Chek:Groups")
       Bot("sendMessage",{"chat_id":chatID,"text":"تم ايجاد ({}) مجموعات خاصه بسورس تشاكي سيتم نقلها الى (<a href=\"http://t.me/nbbot\">NewBot</a>)\nقد يستغرق بعض الوقت".format(Ngp),"reply_to_message_id":message.message_id,"parse_mode":"html","disable_web_page_preview":True})
     else:
       Bot("sendMessage",{"chat_id":chatID,"text":"عذراً لا توجد بيانات خاصه بسورس تشاكي","reply_to_message_id":message.message_id,"parse_mode":"html","disable_web_page_preview":True})
       return False
 
-    groups = redis.smembers("thsake:gog"+BOT_ID)
+    groups = redis.smembers(BOT_ID+"Tshake:Chek:Groups")
     for gp in groups:
       redis.sadd("{}Nbot:groups".format(BOT_ID),gp)
-      ads =redis.smembers('tshake:'+BOT_ID+'mods:'+gp)
-      ows =redis.smembers('tshake:'+BOT_ID+'owners:'+gp)
-      vps =redis.smembers('tshake:'+BOT_ID+'vipgp:'+gp)
+      ads =redis.smembers(BOT_ID+'Tshake:Mod:User'+gp)
+      ows =redis.smembers(BOT_ID+'Tshake:Manager'+gp)
+      vps =redis.smembers(BOT_ID+'Tshake:Special:User'+gp)
       cr = Bot("getChatAdministrators",{"chat_id":gp})
       for c in cr['result']:
         userId = c["user"]["id"]
@@ -45,41 +45,41 @@ def updateMsgs(client, message,redis):
 
       for ow in ows:
         if ow != BOT_ID:
-          redis.sadd("{}Nbot:{}:{}".format(BOT_ID,gp,"admin"),ow)
+          redis.sadd("{}Nbot:{}:{}".format(BOT_ID,gp,"owner"),ow)
 
       for vp in vps:
         if vp != BOT_ID:
-          redis.sadd("{}Nbot:{}:{}".format(BOT_ID,gp,"admin"),vp)
+          redis.sadd("{}Nbot:{}:{}".format(BOT_ID,gp,"vip"),vp)
 
-      if redis.get("lock_tag:tshake"+gp+BOT_ID):
+      if redis.get(BOT_ID+"Tshake:Lock:tagservr"+gp):
         redis.sadd("{}Nbot:Ltag".format(BOT_ID),gp)
-      if redis.get("lock_edit:tshake"+gp+BOT_ID):
+      if redis.get(BOT_ID+"Tshake:Lock:edit"+gp):
         redis.sadd("{}Nbot:Ledits".format(BOT_ID),gp)
-      if redis.get("lock_lllll:tshake"+gp+BOT_ID):
+      if redis.get(BOT_ID+"Tshake:flooding:settings:"+gp):
         redis.sadd("{}Nbot:Lflood".format(BOT_ID),gp)
-      if redis.get("lock_gif:tshake"+gp+BOT_ID):
+      if redis.get(BOT_ID+"Tshake:Lock:Animation"+gp):
         redis.sadd("{}Nbot:Lgifs".format(BOT_ID),gp)
-      if redis.get("lock_files:tshake"+gp+BOT_ID):
+      if redis.get(BOT_ID+"Tshake:Lock:Document"+gp):
         redis.sadd("{}Nbot:Lfiles".format(BOT_ID),gp)
-      if redis.get("lock_mark:tshake"+gp+BOT_ID):
+      if redis.get(BOT_ID+"Tshake:Lock:Markdaun"+gp):
         redis.sadd("{}Nbot:Lmarkdown".format(BOT_ID),gp)
-      if redis.get("lock_photo:tshake"+gp+BOT_ID):
+      if redis.get(BOT_ID+"Tshake:Lock:Photo"+gp):
         redis.sadd("{}Nbot:Lphoto".format(BOT_ID),gp)
-      if redis.get("lock_stecker:tshake"+gp+BOT_ID):
+      if redis.get(BOT_ID+"Tshake:Lock:Sticker"+gp):
         redis.sadd("{}Nbot:Lsticker".format(BOT_ID),gp)
-      if redis.get("lock_video:tshake"+gp+BOT_ID):
+      if redis.get(BOT_ID+"Tshake:Lock:Video"+gp):
         redis.sadd("{}Nbot:Lvideo".format(BOT_ID),gp)
-      if redis.get("lock_audeo:tshake"+gp+BOT_ID):
+      if redis.get(BOT_ID+"Tshake:Lock:Audio"+gp):
         redis.sadd("{}Nbot:Lmusic".format(BOT_ID),gp)
-      if redis.get("lock_voice:tshake"+gp+BOT_ID):
+      if redis.get(BOT_ID+"Tshake:Lock:vico"+gp):
         redis.sadd("{}Nbot:Lvoice".format(BOT_ID),gp)
-      if redis.get("lock_contact:tshake"+gp+BOT_ID):
+      if redis.get(BOT_ID+"Tshake:Lock:Contact"+gp):
         redis.sadd("{}Nbot:Lcontact".format(BOT_ID),gp)
-      if redis.get("lock_fwd:tshake"+gp+BOT_ID):
+      if redis.get(BOT_ID+"Tshake:Lock:forward"+gp):
         redis.sadd("{}Nbot:Lfwd".format(BOT_ID),gp)
-      if redis.get("lock_link:tshake"+gp+BOT_ID):
+      if redis.get(BOT_ID+"Tshake:Lock:Link"+gp):
         redis.sadd("{}Nbot:Llink".format(BOT_ID),gp)
-      if redis.get("lock_username:tshake"+gp+BOT_ID):
+      if redis.get(BOT_ID+"Tshake:Lock:User:Name"+gp):
         redis.sadd("{}Nbot:Lusername".format(BOT_ID),gp)
 
     Bot("sendMessage",{"chat_id":chatID,"text":"تم نقل البيانات من سورس تشاكي الى (<a href=\"http://t.me/nbbot\">NewBot</a>)","reply_to_message_id":message.message_id,"parse_mode":"html","disable_web_page_preview":True})
